215f48b035ac90edc477391545cda496b5d8402e
Matheus Dias <matheushiago0130@gmail.com>
2025-01-20 15:31:33 -0300

    Criação de uma função para atualizar a lista de amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -9,10 +9,24 @@ function adicionarAmigo() {
         listaAmigos.push(nome);
     }
     limparCampo()
-    console.log(listaAmigos);
+    exibirListaDeAmigos();
 }
 
 function limparCampo() {
     campoNome = document.querySelector('input');
     campoNome.value = '';
 }
+
+function exibirListaDeAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; 
+
+    // Loop para iterar sobre o array e criar elementos <li> para cada amigo
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let elementoLista = document.createElement('li'); // O método createElement() cria um elemento <li>
+        elementoLista.innerHTML = listaAmigos[i];
+        lista.appendChild(elementoLista); // O método appendChild adiciona o elemento <li> ao final do elemento selecionado na linha 21 (lista)
+    }
+}
+
+    
